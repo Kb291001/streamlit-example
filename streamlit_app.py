a83dd64e49c3eba13c75b8e47fba78218ea1c922
Kartavaya Bhargava <99725477+Kb291001@users.noreply.github.com>
2023-11-22 16:15:10 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,91 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Title of the app
+# Assuming you have these functions defined to fit your app's logic
+# def create_some_plotly_chart():
+#     # Your code to create a Plotly chart
+#     pass
+
+# def calculate_metrics(budget, objective):
+#     # Your code to calculate metrics based on budget and objective
+#     return metrics
+
 st.title('Media Planning Tool')
+
+# Sidebar for configuration settings
+with st.sidebar:
+    st.header('Configuration')
+    # Add configuration settings here
 
 # User inputs
 st.header('Campaign Objectives and Details')
+campaign_name = st.text_input('Campaign Name')
 objective = st.selectbox('Select the Primary Objective', ['Maximizing Attention', 'Reach', 'Cost Efficiency'])
 budget = st.number_input('Total Campaign Budget', min_value=0.0, value=10000.0)
-# Add more inputs as needed
+ad_format = st.radio("Ad Format", ('Skippable', 'Non-Skippable', 'Bumper'))
+target_audience = st.multiselect('Target Audience', ['Parents', 'Students', 'Professionals', 'Gamers'])
+start_date = st.date_input('Start Date')
+end_date = st.date_input('End Date')
+
+# File uploader
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+if uploaded_file is not None:
+    data = pd.read_csv(uploaded_file)
+    st.write(data)
 
 # Display historical data (Placeholder)
 st.header('Historical Data and Benchmarks')
-st.write('Historical benchmarks would be displayed here.')
+# Sample data for chart
+sample_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['Maximizing Attention', 'Reach', 'Cost Efficiency'])
+st.line_chart(sample_data)
 
 # Recommendations (Placeholder)
 st.header('Recommendations')
-st.write('Based on your inputs, recommendations will be displayed here.')
+if st.button('Calculate Recommendations'):
+    # Assuming calculate_metrics is a function that returns recommendations
+    recommendations = calculate_metrics(budget, objective)
+    st.write(recommendations)
+else:
+    st.write('Based on your inputs, recommendations will be displayed here.')
 
-# Add more interactive elements as needed
+# Layout and Columns
+col1, col2 = st.columns(2)
+with col1:
+    st.subheader('Campaign Details')
+    st.write(f"Name: {campaign_name}")
+    st.write(f"Objective: {objective}")
+    st.write(f"Budget: {budget}")
+    st.write(f"Ad Format: {ad_format}")
+    st.write(f"Target Audience: {', '.join(target_audience)}")
+    st.write(f"Duration: {start_date} to {end_date}")
+
+with col2:
+    st.subheader('Historical Campaigns')
+    # Placeholder for historical campaigns table/chart
+    st.write('Historical campaigns data would be displayed here.')
+
+# Use expander to organize content
+with st.expander("See explanation"):
+    st.write("""
+        The chart above shows some really cool data about your campaign.
+        """)
+
+# Custom styling
+st.markdown(
+    """
+    <style>
+    .big-font {
+        font-size:300% !important;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+st.markdown('<p class="big-font">Media Planning Tool</p>', unsafe_allow_html=True)
+
+# Run the app
+if __name__ == '__main__':
+    st.run()
